1c0892b4d0833d8a7ebbd4ad97390ede269ad059
MARS <sdamar321@gmail.com>
2023-06-11 22:28:16 +0700

    Initial commit

---
+++ app/src/main/java/com/infinitelearning/viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.infinitelearning.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    var angka = 0
+
+    fun tambahAngka(){
+        angka++
+    }
+}
